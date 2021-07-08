40c793714740e8ad75eadbb8bb08025f9953d99b
Sonika <sonikapoudyal5@gmail.com>
2021-07-08 09:22:34 -0400

    comment.js created

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Vote = require('./Vote');
 const Post = require('./Post');
 const User = require('./User');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -40,5 +41,21 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
 
